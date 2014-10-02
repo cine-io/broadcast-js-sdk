a5703ea063c997cd14d85d50160f942ce88aac60
Thomas Shafer <thomasjshafer@gmail.com>
2014-10-01 21:38:46 -0700

    update to 0.1.6

--- build/cineio-dev.js
+++ build/cineio-dev.js
@@ -148,7 +148,7 @@ requiresInit = function() {
 noop = function() {};
 
 CineIO = {
-  version: "0.1.5",
+  version: "0.1.6",
   config: {},
   init: function(publicKey, options) {
     var prop, value, _results;
