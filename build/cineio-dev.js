5d4f9b98c0c42296584ac7d64ceb1d4126c7e506
Thomas Shafer <thomasjshafer@gmail.com>
2014-11-11 08:47:49 -0800

    version bump

--- build/cineio-dev.js
+++ build/cineio-dev.js
@@ -154,7 +154,7 @@ requiresInit = function() {
 noop = function() {};
 
 CineIO = {
-  version: "0.2.4",
+  version: "0.2.5",
   config: {},
   init: function(publicKey, options) {
     var prop, value, _results;
--- src/main.coffee
+++ src/main.coffee
@@ -3,7 +3,7 @@ requiresInit = ->
 noop = ->
 
 CineIO =
-  version: "0.2.4"
+  version: "0.2.5"
   config: {}
   init: (publicKey, options)->
     throw new Error("Public Key required") unless publicKey
